3a35084bad9f45ef334faba1d304d94d1a727d3d
kowalus23 <kowalus23@gmail.com>
2019-07-17 19:57:42 +0200

    Updates

---
+++ precache-manifest.3329a4b9cd8821f4df353790b376a652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa4f52e883dd6816f8d803a691398d1",
+    "url": "/weather-forecast-browser/index.html"
+  },
+  {
+    "revision": "30f7ae2d73253743cb03",
+    "url": "/weather-forecast-browser/static/css/main.4f999516.chunk.css"
+  },
+  {
+    "revision": "87338747af178444576b",
+    "url": "/weather-forecast-browser/static/js/2.4112612a.chunk.js"
+  },
+  {
+    "revision": "30f7ae2d73253743cb03",
+    "url": "/weather-forecast-browser/static/js/main.34e3b242.chunk.js"
+  },
+  {
+    "revision": "f8f3e25c2027c71b084d",
+    "url": "/weather-forecast-browser/static/js/runtime~main.a332aea5.js"
+  }
+]);
